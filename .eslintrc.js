deb847e8fb85f205b5e4a214cb3b43ec8ed8ddec
rsek <hello@rsek.ca>
2022-10-12 17:31:32 -0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,22 @@ module.exports = {
   env: {
     es6: true
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-  parser: '@typescript-eslint/parser',
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: './test-d/tsconfig.json',
-    sourceType: 'module'
+    project: "./test-d/tsconfig.json",
+    sourceType: "module"
   },
-  plugins: ['@typescript-eslint', 'eslint-plugin-tsdoc'],
+  plugins: ["@typescript-eslint", "eslint-plugin-tsdoc"],
   rules: {
-    'tsdoc/syntax': 'warn',
-    '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
+    "tsdoc/syntax": "warn",
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "lines-between-class-members": "off",
+    "@typescript-eslint/lines-between-class-members": "error",
+    "space-before-blocks": "off",
+    "@typescript-eslint/space-before-blocks": "error",
+    "padding-line-between-statements": "off",
+    "@typescript-eslint/padding-line-between-statements": "error"
   }
 };
